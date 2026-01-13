3e5c811da859e791945cef82fcfa0ac8dff8ed99
Olalekan Adeyemi <olalekan.adeyemi@enbridge.com>
2026-01-13 22:43:47 +0000

    python-app deployment using ArgoCD with kubenetes

--- src/app.py
+++ src/app.py
@@ -1,28 +1,27 @@
-
 from flask import Flask, jsonify
 import datetime
 import socket
 
+
 app = Flask(__name__)
 
-@app.route('/api/vi/details')
 
-def hello_world():
+@app.route('/api/v1/info')
+
+def info():
     return jsonify({
-       'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
-       'hostname': socket.gethostname()
+    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes'
     })
 
-@app.route('/api/vi/healthz')
+@app.route('/api/v1/healthz')
 
 def health():
+	# Do an actual check here
     return jsonify({'status': 'up'}), 200
 
 if __name__ == '__main__':
-    
+
     app.run(host="0.0.0.0")
-
-
-
-#'/api/vi/details'
-#'/api/vi/healthz'
