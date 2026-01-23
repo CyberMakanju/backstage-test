05ac8c27cf9d018c8cfd612d27db9f4ed5908850
Olalekan Adeyemi <olalekan.adeyemi@enbridge.com>
2026-01-23 03:18:03 +0000

    fixed cd

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, human@@!!1992! <3',
+        'message': 'You are doing great, human@@!!1994! <3',
         'deployed_on': 'kubernetes'
     })
 
